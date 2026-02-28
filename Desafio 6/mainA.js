7d595dd00b7635f35b3ed207a0e0b268945faf85
Agustina Elola <agustina.flor.elola@gmail.com>
2026-02-28 19:52:38 -0300

    Desafio 6 - ej

---
+++ Desafio 6/mainA.js
@@ -0,0 +1,13 @@
+// EJERCICIO A
+var cantidadDeGatos = 10;
+
+for(var i=1; i<= cantidadDeGatos; i++){
+    if(i%3==1){
+        console.log('Gato #'+ i + ': 😺')
+    }else if(i%3 == 2){
+        console.log('Gato #'+ i + ': 😸')
+    }else if(i%3==0){
+        console.log('Gato #'+ i + ': 😹')
+    }
+}
+
